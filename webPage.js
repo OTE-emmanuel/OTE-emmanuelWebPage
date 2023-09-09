b7859c17bc5813794a7013fdec6338e066ffcf15
Emmanuel Onipede <onipedeemmanuel74@gmail.com>
2023-09-09 16:30:02 -0700

    first upload

---
+++ webPage.js
@@ -0,0 +1,11 @@
+let currentUTCTime = document.getElementById("currentUTCTime");
+    let currentDayOfTheWeek = document.getElementById("currentDayOfTheWeek");
+
+    setInterval(() => {
+        const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        
+        let currentTime = new Date();
+
+        currentUTCTime.innerHTML = (currentTime.valueOf())
+        currentDayOfTheWeek.innerHTML = daysOfTheWeek[(currentTime.getDay())]
+    }, 1)
